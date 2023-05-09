0fc0ce51cd6948d64e84c5c5ccd09e5e8839f625
bdevops99 <bdevops99@gmail.com>
2023-05-09 21:47:55 +0530

    ansible learn

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
+  type  = "SecureString"
   value = "Hello World"
+  key_id = "50f72021-fbd2-4087-bb3f-7782291eaff6"
 }
