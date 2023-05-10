4972c1d140ed1154ad559e8af84a614f47b317de
bdevops99 <bdevops99@gmail.com>
2023-05-10 11:06:17 +0530

    ansible learn

---
+++ env-dev/main.tfvars
@@ -0,0 +1,5 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.panda4u.online:8080/" }
+  { name = "dev.frontend.user_url", value = "http://user-dev.panda4u.online:8080/" }
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.panda4u.online:8080/" }
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "SecureString"
-  value = "Hello World"
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
+  value = var.parameters[count.index].value
+  type  = "String"
   key_id = "50f72021-fbd2-4087-bb3f-7782291eaff6"
 }
