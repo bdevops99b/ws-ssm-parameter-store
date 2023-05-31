4a5f1007d2f5fbeb05b2f2807f7216adb87b24d3
bdevops99 <bdevops99@gmail.com>
2023-05-31 09:49:52 +0530

    final tests

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameter" {
   name = var.parameters[count.index].name
   value = var.parameters[count.index].value
   type  = "String"
-  key_id = "50f72021-fbd2-4087-bb3f-7782291eaff6"
+  key_id = "0e9d2058-45b4-40b9-8eb3-677118252e17"
 }
 
 resource "aws_ssm_parameter" "passwords" {
@@ -11,5 +11,5 @@ resource "aws_ssm_parameter" "passwords" {
   name = var.passwords[count.index].name
   value = var.passwords[count.index].value
   type  = "SecureString"
-  key_id = "50f72021-fbd2-4087-bb3f-7782291eaff6"
+  key_id = "0e9d2058-45b4-40b9-8eb3-677118252e17"
 }
